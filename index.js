b8411c4f2af7695b9a8c764eebd8648fe68c2003
lrmn7 <lrmnid@gmail.com>
2023-07-14 22:08:33 +0700

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+require('./Structures/WaifuMusic');
+const express = require('express');
+const app = express();
+
+// Set up routes
+app.get('/', (req, res) => {
+  res.send('Dev L RMN!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
